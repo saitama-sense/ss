2e49a2cf8dfd931afe5b87fc402fa01603b621d0
Saitama <115386314+saitama-sense@users.noreply.github.com>
2022-10-23 21:24:42 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-Project/local_manifest --depth 1 -b rosy-PE-plus-12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b ten-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/saitama-sense/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=No Need
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
+lunch bootleg_whyred-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=rosy
-export KBUILD_BUILD_HOST=nfsproject
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=nfsproject
-mka bacon -j8
-# Switch to vanilla build
+export KBUILD_BUILD_USER=Saitama
+export KBUILD_BUILD_HOST=Cirrus
+export BUILD_USERNAME=Saitama
+export BUILD_HOSTNAME=Cirrus
+# export USE_MICROG=true
+# export TARGET_USES_MINI_GAPPS=true
+mka bacon-j8
+# build
